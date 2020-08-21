cdf21c92388ceeae285c24f205df823b038d6405
vverzun <vladyslav.verzun@gmail.com>
2020-08-21 22:10:41 +0300

    Update App container

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import RootLayout from '../../layout';
+import Header from '../Header';
+import Home from '../Home';
+import './style.module.scss';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Publica',
+    fontSize: 16
+  }
+});
 
 const App = () => (
-  <div>Initialize</div>
+  <ThemeProvider theme={theme}>
+    <RootLayout
+      header={
+        <Header />
+      }
+      content={
+        <Home />
+      }
+    />
+  </ThemeProvider>
 );
 
 export default App;
