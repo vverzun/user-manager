475f1e85a2e9b056b9ceef973a00fd53e54f606f
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2020-12-12 23:21:44 +0300

    Add redirect on event click

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import _ from 'lodash';
 import moment from 'moment';
+import { useHistory } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -10,12 +11,18 @@ import style from './style.module.scss';
 import eventList from '../../mockData/eventList';
 
 const EventList = () => {
+  const history = useHistory();
+  const handleViewEventDetails = useCallback(id => () => {
+    history.push(`/event/${id}`);
+  }, []);
+
   const renderEvents = useCallback(() => (
     _.map(eventList, ({ title, date, location, id }) => (
       <Card
         key={id}
         variant="outlined"
         className={style.eventCard}
+        onClick={handleViewEventDetails(id)}
       >
         <CardContent>
           <Typography variant="h6">{title}</Typography>
