ded645c87174bd8a05c0a195b531c93a5958a5e5
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2020-12-12 22:52:46 +0300

    Add router

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Event from '../../components/Event';
+import Router from '../../router';
 import store from '../../store';
 import './style.module.scss';
 
@@ -12,22 +12,10 @@ const theme = createMuiTheme({
   }
 });
 
-const description = `
-The party will take plave in the flat. Bring your flip
-flops with u because the floor is dirty, have a nice party. WooooHoooo!!
-`;
-
 const App = () => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <Event
-        id="1"
-        title="Party"
-        date={new Date().toDateString()}
-        description={description}
-        participants={['Vanya', 'Natalie', 'Vera', 'Igor']}
-        location="Lybidska st."
-      />
+      <Router />
     </ThemeProvider>
   </Provider>
 );
