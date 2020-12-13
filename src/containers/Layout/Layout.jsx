238b667af1e5fccbfc681692115e6abbeb3ffb7b
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2020-12-13 22:10:20 +0300

    Fix header styling

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -12,7 +12,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ErrorAlert from '../../components/ErrorAlert';
-import styles from './styles.modules.scss';
+import style from './styles.modules.scss';
 
 const Layout = ({ children }) => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -35,34 +35,35 @@ const Layout = ({ children }) => {
   };
 
   return (
-    <Box className={styles.layout}>
+    <Box>
       <AppBar position="static">
-        <Toolbar className={styles.toolbar}>
-          <Button
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-            className={styles.menuButton}
-          >
-            <MenuIcon className={styles.menuIcon} />
-          </Button>
+        <Toolbar className={style.toolbar}>
+          <Box className={style.menu}>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <MenuIcon className={style.icon} />
+            </Button>
 
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={redirect('/')}>Home</MenuItem>
-            <MenuItem onClick={redirect('/profile')}>Profile</MenuItem>
-            <MenuItem onClick={redirect('/events')}>Events</MenuItem>
-            <MenuItem>Logout</MenuItem>
-          </Menu>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={redirect('/')}>Home</MenuItem>
+              <MenuItem onClick={redirect('/profile')}>Profile</MenuItem>
+              <MenuItem onClick={redirect('/events')}>Events</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </Menu>
 
-          <Typography variant="h6" className={styles.headerTitle}>
-            Partynder
-          </Typography>
+            <Typography variant="h6">
+              Partynder
+            </Typography>
+          </Box>
           <Button color="inherit" onClick={handleLoginRedirect}>Login</Button>
         </Toolbar>
       </AppBar>
