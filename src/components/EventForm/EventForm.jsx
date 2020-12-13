e8a9bc39fb3478905b47dff0423c2ebd4403c3d5
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2020-12-14 01:34:36 +0300

    Add selector for eventType

--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -1,35 +1,41 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import { loadCreateEvent, closeModalAction } from '../../store/actions';
 
+const PARTY_TYPES = ['DRINKING', 'SPORTS', 'MOVIES', 'ACTION', 'READING', 'BIBLE_STUDY', 'OTHER'];
+
 const EventForm = ({ title }) => {
   const dispatch = useDispatch();
-  const userId = useSelector(state => state.MOCK_USER_ID);
+  const username = useSelector(state => state.MOCK_USER_name);
 
   const [formData, setFormData] = useState({
     title: '',
+    partyType: 'OTHER',
     eventDate: '2017-05-24T10:30',
     creationDate: new Date(),
     location: '',
     description: '',
     duration: '',
     radius: '',
-    createdBy: { id: userId },
-    participants: [{ id: userId }]
+    createdBy: { name: username },
+    participants: [{ name: username }]
   });
 
   const handleInputChange = useCallback(event => {
     event.persist();
     setFormData(prevFormData => ({
       ...prevFormData,
-      [event.target.id]: event.target.value
+      [event.target.name]: event.target.value
     }));
   }, [formData]);
 
@@ -41,19 +47,35 @@ const EventForm = ({ title }) => {
     dispatch(loadCreateEvent(formData));
   };
 
+  const menuOptions = useMemo(() => (
+    PARTY_TYPES.map(type => (
+      <MenuItem value={type} key={uuidv4()}>
+        {type}
+      </MenuItem>
+    ))
+  ), []);
+
   return (
     <>
-      <DialogTitle id="form-dialog-title">{ title }</DialogTitle>
+      <DialogTitle name="form-dialog-title">{ title }</DialogTitle>
       <DialogContent>
         <TextField
-          id="title"
+          name="title"
           label="Title"
           fullWidth
           onChange={handleInputChange}
           value={formData.title}
         />
+        <Select
+          onChange={handleInputChange}
+          fullWidth
+          name="partyType"
+          value={formData.partyType}
+        >
+          {menuOptions}
+        </Select>
         <TextField
-          id="location"
+          name="location"
           label="Location"
           fullWidth
           onChange={handleInputChange}
@@ -61,7 +83,7 @@ const EventForm = ({ title }) => {
         />
         <TextField
           type="number"
-          id="duration"
+          name="duration"
           label="Duration"
           fullWidth
           onChange={handleInputChange}
@@ -69,14 +91,14 @@ const EventForm = ({ title }) => {
         />
         <TextField
           type="number"
-          id="radius"
+          name="radius"
           label="Radius"
           fullWidth
           onChange={handleInputChange}
           value={formData.radius}
         />
         <TextField
-          id="eventDate"
+          name="eventDate"
           label="Time"
           type="datetime-local"
           InputLabelProps={{
@@ -88,7 +110,7 @@ const EventForm = ({ title }) => {
         <TextField
           multiline
           rowsMax={4}
-          id="description"
+          name="description"
           label="Description"
           fullWidth
           onChange={handleInputChange}
